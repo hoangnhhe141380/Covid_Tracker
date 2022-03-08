fff35c84f7894a8b97c5761a8483cf887ba85914
Your Name <you@example.com>
2022-03-08 22:03:50 +0700

    20220308 - Update layout round 3

--- app/src/main/java/com/example/covid_tracker/adapter/SymptomsAdapter.java
+++ app/src/main/java/com/example/covid_tracker/adapter/SymptomsAdapter.java
@@ -1,4 +1,66 @@
 package com.example.covid_tracker.adapter;
 
-public class SymptomsAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.covid_tracker.R;
+import com.example.covid_tracker.model.Symptom;
+
+import java.util.List;
+
+public class SymptomsAdapter extends RecyclerView.Adapter<SymptomsAdapter.SymptomsViewHolder>{
+
+    private List<Symptom> listSymptoms;
+
+
+    public SymptomsAdapter(List<Symptom> listSymptoms) {
+        this.listSymptoms = listSymptoms;
+    }
+
+    @NonNull
+    @Override
+    public SymptomsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.layout_symptoms_data, parent, false);
+        return new SymptomsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SymptomsViewHolder holder, int position) {
+        Symptom symptom = listSymptoms.get(position);
+        if (symptom == null) return;
+
+
+        holder.i_symptom.setImageResource(symptom.getImage());
+        holder.tv_title.setText(symptom.getTitle());
+        holder.tv_description.setText(symptom.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listSymptoms != null ? listSymptoms.size() : 0;
+    }
+
+    public class SymptomsViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView i_symptom;
+        private TextView tv_title;
+        private TextView tv_description;
+
+        public SymptomsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            i_symptom = itemView.findViewById(R.id.i_symptom);
+            tv_title = itemView.findViewById(R.id.tv_symptom_title);
+            tv_description = itemView.findViewById(R.id.tv_symptom_description);
+
+
+        }
+    }
 }
--- app/src/main/java/com/example/covid_tracker/fragments/FragmentSymptoms.java
+++ app/src/main/java/com/example/covid_tracker/fragments/FragmentSymptoms.java
@@ -1,5 +1,6 @@
 package com.example.covid_tracker.fragments;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,18 +9,77 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.covid_tracker.MainActivity;
 import com.example.covid_tracker.R;
+import com.example.covid_tracker.adapter.SymptomsAdapter;
+import com.example.covid_tracker.model.Symptom;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentSymptoms extends Fragment {
 
+    private final int NUMBER_OF_COLUMN = 2;
+
     private RecyclerView recyclerView;
+    private View view;
+    private MainActivity mainActivity;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().setTitle("Symptoms");
-        return inflater.inflate(R.layout.layout_fragment_symptoms, container, false);
+        view = inflater.inflate(R.layout.layout_fragment_symptoms, container, false);
+
+        mainActivity = (MainActivity) getActivity();
+
+        recyclerView = view.findViewById(R.id.rcv_symptoms);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(mainActivity, NUMBER_OF_COLUMN);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        SymptomsAdapter adapter = new SymptomsAdapter(getListSymptoms());
+        recyclerView.setAdapter(adapter);
+
+        return view;
+    }
+
+    //Handle add data for recycle symptoms view
+    private List<Symptom> getListSymptoms() {
+        List<Symptom> listSymptoms = new ArrayList<>();
+        listSymptoms.add(new Symptom(R.drawable.i_s1, "Coughing & Sneezing", "Lorem abasdgasdgasdasgasdggsdagasdgasdgsdf"));
+        listSymptoms.add(new Symptom(R.drawable.i_s2, "Strong Headache", "Lorem abasdf"));
+        listSymptoms.add(new Symptom(R.drawable.i_s3, "Shortness Of Breath", "Lorem abasdf"));
+        listSymptoms.add(new Symptom(R.drawable.i_s4, "High Fever", "Lorem abasdf"));
+        listSymptoms.add(new Symptom(R.drawable.i_s5, "Sore Throat", "Lorem abasasdgasdgasgdgasdsgdsgdasgdsgagsddf"));
+        listSymptoms.add(new Symptom(R.drawable.i_s6, "Kidney Failure", "Lorem abasdf"));
+        return listSymptoms;
+    }
+
+    //Handle column spacing for grid layout
+    public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
+        private int space;
+
+        public SpacesItemDecoration(int space) {
+            this.space = space;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view,
+                                   RecyclerView parent, RecyclerView.State state) {
+            outRect.left = space;
+            outRect.right = space;
+            outRect.bottom = space;
+
+            // Add top margin only for the first item to avoid double space between items
+            if (parent.getChildLayoutPosition(view) == 0) {
+                outRect.top = space;
+            } else {
+                outRect.top = 0;
+            }
+        }
     }
 }
