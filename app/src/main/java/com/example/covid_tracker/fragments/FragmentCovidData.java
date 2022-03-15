2745e69611f23378e1d1bf53d73c114d72283b54
Your Name <you@example.com>
2022-03-15 22:38:25 +0700

    20220315 - Update FragmentCovidData round 1

--- app/src/main/java/com/example/covid_tracker/fragments/FragmentCovidData.java
+++ app/src/main/java/com/example/covid_tracker/fragments/FragmentCovidData.java
@@ -4,19 +4,88 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.covid_tracker.GetDataFromJson;
+import com.example.covid_tracker.MainActivity;
 import com.example.covid_tracker.R;
+import com.example.covid_tracker.adapter.CountryPickerAdapter;
+import com.example.covid_tracker.model.Summary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class FragmentCovidData extends Fragment {
+
+    private final String SUMMARY_URL = "https://corona.lmao.ninja/v2/";
+
+    private List<Summary> listSummary = new ArrayList<>();
+    private Spinner spinner;
+    private CountryPickerAdapter countryPickerAdapter;
+    private MainActivity mainActivity;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().setTitle("Covid Data");
-        return inflater.inflate(R.layout.layout_fragment_covid_data, container, false);
+
+        View view = inflater.inflate(R.layout.layout_fragment_covid_data, container, false);
+
+        spinner = view.findViewById(R.id.spinner_country);
+        mainActivity = (MainActivity) getActivity();
+
+        countryPickerAdapter = new CountryPickerAdapter(mainActivity, R.layout.item_country_selected, getListCountry());
+        spinner.setAdapter(countryPickerAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(mainActivity,
+                        countryPickerAdapter.getItem(i).getCountry(),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        return view;
+    }
+
+    public List<Summary> getListCountry() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(SUMMARY_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        GetDataFromJson getDataFromJson = retrofit.create(GetDataFromJson.class);
+
+        Call<List<Summary>> call = getDataFromJson.getDataSummary();
+
+        call.enqueue(new Callback<List<Summary>>() {
+            @Override
+            public void onResponse(Call<List<Summary>> call, Response<List<Summary>> response) {
+                listSummary = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<List<Summary>> call, Throwable t) {
+                listSummary = null;
+            }
+        });
+
+        return listSummary;
     }
 }
