16dd45ade9028331ad5a470d3f6e0c1d1154a00a
dult6331 <88769860+dult6331@users.noreply.github.com>
2022-03-21 22:56:35 +0700

    Du update layout fragment profile + add edit function fragment profile

--- app/src/main/java/com/example/covid_tracker/fragments/FragmentProfile.java
+++ app/src/main/java/com/example/covid_tracker/fragments/FragmentProfile.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -24,7 +25,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class FragmentProfile extends Fragment {
-    private static final String TAG ="lmao";
+    private static final String TAG = "lmao";
     private FirebaseDatabase database;
     private DatabaseReference ref;
     private EditText edt_name;
@@ -32,12 +33,24 @@ public class FragmentProfile extends Fragment {
     private EditText edit_bd;
     private EditText edit_gender;
     private EditText edit_phone;
+    private EditText edt_vaccine_name1;
+    private EditText edt_vaccine_unit1;
+    private EditText edt_vaccine_type1;
+    private EditText edt_vaccine_name2;
+    private EditText edt_vaccine_unit2;
+    private EditText edt_vaccine_type2;
+    private EditText edt_vaccine_name3;
+    private EditText edt_vaccine_unit3;
+    private EditText edt_vaccine_type3;
+    private Button btnEdit;
+    private Button btnSave;
+    private Button btnCancel;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_fragment_profile, container, false);
-        String phoneNumber = null;
+        String  phoneNumber = "+84913556456";
         Bundle bundle = this.getArguments();
         if (null != bundle) {
             phoneNumber = bundle.getString("phoneNumber");
@@ -50,7 +63,85 @@ public class FragmentProfile extends Fragment {
         ref = bindingDatabaseRef(phoneNumber);
         bindingAccountListener(ref);
         bindingView(view);
+        bindingAction();
         return view;
+    }
+
+    private void bindingAction() {
+        btnEdit.setOnClickListener(this::onEditPrf);
+        btnSave.setOnClickListener(this::onSavePrf);
+        btnCancel.setOnClickListener(this::onCancelPrf);
+    }
+
+    private void onEditPrf(View view) {
+        edt_name.setEnabled(true);
+        edit_phone.setEnabled(true);
+        edt_vaccine_name1.setEnabled(true);
+        edt_vaccine_unit1.setEnabled(true);
+        edt_vaccine_type1.setEnabled(true);
+        edt_vaccine_name2.setEnabled(true);
+        edt_vaccine_unit2.setEnabled(true);
+        edt_vaccine_type2.setEnabled(true);
+        edt_vaccine_name3.setEnabled(true);
+        edt_vaccine_unit3.setEnabled(true);
+        edt_vaccine_type3.setEnabled(true);
+        btnEdit.setVisibility(View.GONE);
+        btnCancel.setVisibility(View.VISIBLE);
+        btnSave.setVisibility(View.VISIBLE);
+    }
+
+    private void onSavePrf(View view) {
+        updateData();
+        ref.setValue(MainActivity.currentAccount);
+        edt_name.setEnabled(false);
+        edit_phone.setEnabled(false);
+        edt_vaccine_name1.setEnabled(false);
+        edt_vaccine_unit1.setEnabled(false);
+        edt_vaccine_type1.setEnabled(false);
+        edt_vaccine_name2.setEnabled(false);
+        edt_vaccine_unit2.setEnabled(false);
+        edt_vaccine_type2.setEnabled(false);
+        edt_vaccine_name3.setEnabled(false);
+        edt_vaccine_unit3.setEnabled(false);
+        edt_vaccine_type3.setEnabled(false);
+        btnEdit.setVisibility(View.VISIBLE);
+        btnCancel.setVisibility(View.GONE);
+        btnSave.setVisibility(View.GONE);
+        bindingData();
+    }
+
+    private void onCancelPrf(View view) {
+        edt_name.setEnabled(false);
+        edit_phone.setEnabled(false);
+        edt_vaccine_name1.setEnabled(false);
+        edt_vaccine_unit1.setEnabled(false);
+        edt_vaccine_type1.setEnabled(false);
+        edt_vaccine_name2.setEnabled(false);
+        edt_vaccine_unit2.setEnabled(false);
+        edt_vaccine_type2.setEnabled(false);
+        edt_vaccine_name3.setEnabled(false);
+        edt_vaccine_unit3.setEnabled(false);
+        edt_vaccine_type3.setEnabled(false);
+        btnEdit.setVisibility(View.VISIBLE);
+        btnCancel.setVisibility(View.GONE);
+        btnSave.setVisibility(View.GONE);
+    }
+
+    private void updateData() {
+        MainActivity.currentAccount.setName(edt_name.getText().toString().trim());
+        MainActivity.currentAccount.setPhone(edit_phone.getText().toString().trim());
+        MainActivity.currentAccount.setVaccine1(new Vaccine(
+                edt_vaccine_type1.getText().toString().trim(),
+                edt_vaccine_unit1.getText().toString().trim(),
+                edt_vaccine_name1.getText().toString().trim()));
+        MainActivity.currentAccount.setVaccine2(new Vaccine(
+                edt_vaccine_type2.getText().toString().trim(),
+                edt_vaccine_unit2.getText().toString().trim(),
+                edt_vaccine_name2.getText().toString().trim()));
+        MainActivity.currentAccount.setVaccine3(new Vaccine(
+                edt_vaccine_type3.getText().toString().trim(),
+                edt_vaccine_unit3.getText().toString().trim(),
+                edt_vaccine_name3.getText().toString().trim()));
     }
 
     private DatabaseReference bindingDatabaseRef(String phoneNumber) {
@@ -72,6 +163,18 @@ public class FragmentProfile extends Fragment {
         edit_bd = view.findViewById(R.id.edt_bd);
         edit_gender = view.findViewById(R.id.edt_gender);
         edit_phone = view.findViewById(R.id.edt_phone);
+        edt_vaccine_name1 = view.findViewById(R.id.edt_vaccine_name1);
+        edt_vaccine_unit1 = view.findViewById(R.id.edt_vaccine_unit1);
+        edt_vaccine_type1 = view.findViewById(R.id.edt_vaccine_type1);
+        edt_vaccine_name2 = view.findViewById(R.id.edt_vaccine_name2);
+        edt_vaccine_unit2 = view.findViewById(R.id.edt_vaccine_unit2);
+        edt_vaccine_type2 = view.findViewById(R.id.edt_vaccine_type2);
+        edt_vaccine_name3 = view.findViewById(R.id.edt_vaccine_name3);
+        edt_vaccine_unit3 = view.findViewById(R.id.edt_vaccine_unit3);
+        edt_vaccine_type3 = view.findViewById(R.id.edt_vaccine_type3);
+        btnEdit = view.findViewById(R.id.btn_edit_prf);
+        btnSave = view.findViewById(R.id.btn_save_prf);
+        btnCancel = view.findViewById(R.id.btn_cancel_prf);
     }
 
     private void bindingData() {
@@ -80,6 +183,36 @@ public class FragmentProfile extends Fragment {
         edit_bd.setText(MainActivity.currentAccount.getBirthDate());
         edit_gender.setText(MainActivity.currentAccount.isGender() ? "Male" : "Female");
         edit_phone.setText(MainActivity.currentAccount.getPhone());
+        if (null != MainActivity.currentAccount.getVaccine1()) {
+            edt_vaccine_name1.setText(MainActivity.currentAccount.getVaccine1().getVaccinationType());
+            edt_vaccine_unit1.setText(MainActivity.currentAccount.getVaccine1().getVaccinationUnit());
+            edt_vaccine_type1.setText(MainActivity.currentAccount.getVaccine1().getDate());
+        } else {
+            edt_vaccine_name1.setText("N/A");
+            edt_vaccine_unit1.setText("N/A");
+            edt_vaccine_type1.setText("N/A");
+        }
+
+        if (null != MainActivity.currentAccount.getVaccine2()) {
+            edt_vaccine_name2.setText(MainActivity.currentAccount.getVaccine2().getVaccinationType());
+            edt_vaccine_unit2.setText(MainActivity.currentAccount.getVaccine2().getVaccinationUnit());
+            edt_vaccine_type2.setText(MainActivity.currentAccount.getVaccine2().getDate());
+        } else {
+            edt_vaccine_name2.setText("N/A");
+            edt_vaccine_unit2.setText("N/A");
+            edt_vaccine_type2.setText("N/A");
+        }
+
+        if (null != MainActivity.currentAccount.getVaccine3()) {
+            edt_vaccine_name3.setText(MainActivity.currentAccount.getVaccine3().getVaccinationType());
+            edt_vaccine_unit3.setText(MainActivity.currentAccount.getVaccine3().getVaccinationUnit());
+            edt_vaccine_type3.setText(MainActivity.currentAccount.getVaccine3().getDate());
+        } else {
+            edt_vaccine_name3.setText("N/A");
+            edt_vaccine_unit3.setText("N/A");
+            edt_vaccine_type3.setText("N/A");
+        }
+
     }
 
     private void bindingAccountListener(DatabaseReference reference) {
@@ -89,45 +222,42 @@ public class FragmentProfile extends Fragment {
                 for (DataSnapshot data : snapshot.getChildren()
                 ) {
                     switch (data.getKey()) {
-                        case "Address": {
+                        case "address": {
                             MainActivity.currentAccount.setAddress(data.getValue().toString());
                             break;
                         }
-                        case "Gender": {
+                        case "gender": {
                             MainActivity.currentAccount.setGender(data.getValue().toString().equals(1));
                             break;
                         }
-                        case "Name": {
+                        case "name": {
                             MainActivity.currentAccount.setName(data.getValue().toString());
                             break;
                         }
-                        case "Phone": {
+                        case "phone": {
                             MainActivity.currentAccount.setPhone(data.getValue().toString());
                             break;
                         }
-                        case "PersonalID": {
+                        case "personalID": {
                             MainActivity.currentAccount.setPersonalID(data.getValue().toString());
                             break;
                         }
-                        case "BirthDate": {
+                        case "birthDate": {
                             MainActivity.currentAccount.setBirthDate(data.getValue().toString());
                             break;
                         }
-//                        case "Vaccine1": {
-//                            account.setVaccine1(data.getValue(Vaccine.class));
-//                            Log.e(TAG, account.getVaccine1().getDate());
-//                            break;
-//                        }
-//                        case "Vaccine2": {
-//                            account.setVaccine2(data.getValue(Vaccine.class));
-//                            Log.e(TAG, account.getVaccine2().getDate());
-//                            break;
-//                        }
-//                        case "Vaccine3": {
-//                            account.setVaccine3(data.getValue(Vaccine.class));
-//                            Log.e(TAG, account.getVaccine3().getDate());
-//                            break;
-//                        }
+                        case "vaccine1": {
+                            MainActivity.currentAccount.setVaccine1(data.getValue(Vaccine.class));
+                            break;
+                        }
+                        case "vaccine2": {
+                            MainActivity.currentAccount.setVaccine2(data.getValue(Vaccine.class));
+                            break;
+                        }
+                        case "vaccine3": {
+                            MainActivity.currentAccount.setVaccine3(data.getValue(Vaccine.class));
+                            break;
+                        }
                     }
                 }
                 bindingData();
